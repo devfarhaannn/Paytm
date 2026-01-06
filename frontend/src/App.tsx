5bcc6bb18ce3ad06cfe3431318f943802f9953dd
devfarhaannn <bhattfarhan1804@gmail.com>
2026-01-07 00:34:31 +0530

    init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import Dashboard from './pages/Dashboard'
@@ -12,6 +12,7 @@ function App() {
     <>
      <BrowserRouter>
      <Routes>
+      <Route path='/' element={<Navigate to = "/signup" replace></Navigate>}/>
       <Route path='/signup' element={<Signup/>}/>
       <Route path='/signin' element={<Signin/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
