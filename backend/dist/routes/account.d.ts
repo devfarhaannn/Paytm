7cc5588a0ffb199c9d08cee74f56974ab665138b
devfarhaannn <bhattfarhan1804@gmail.com>
2026-05-09 21:45:30 +0530

    init

--- backend/dist/routes/account.d.ts
+++
@@ -1,3 +0,0 @@
-declare const accountRouter: import("express-serve-static-core").Router;
-export default accountRouter;
-//# sourceMappingURL=account.d.ts.map
--- backend/dist/routes/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const rootRouter: import("express-serve-static-core").Router;
-//# sourceMappingURL=index.d.ts.map
