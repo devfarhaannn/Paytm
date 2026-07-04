24bef0971499b34c05478add620bc10c8bb8563a
devfarhaannn <bhattfarhan1804@gmail.com>
2026-07-04 12:33:49 +0530

    add some features

--- backend/src/routes/account.ts
+++ backend/src/routes/account.ts
@@ -22,39 +22,46 @@ accountRouter.get("/balance", authMidlleware, async (req: Request, res: Response
 accountRouter.post("/transfer", authMidlleware, async (req: Request, res: Response) => {
     const session = await mongoose.startSession();
 
-    session.startTransaction();
-    const { amount, to } = req.body
+    try {
+        session.startTransaction();
+        const { amount, to } = req.body
 
-    const account = await AccountModel.findOne({
-        userId: (req as any).userId
-    }).session(session);
+        const account = await AccountModel.findOne({
+            userId: (req as any).userId
+        }).session(session);
 
-    if (!account || account.balance < amount) {
-        await session.abortTransaction()
-        res.status(400).json({
-            message: "Invalid account or Insufficient balance"
+        if (!account || account.balance < amount) {
+            await session.abortTransaction()
+            return res.status(400).json({
+                message: "Invalid account or Insufficient balance"
+            })
+        }
+
+        const toAccount = await AccountModel.findOne({
+            userId: to
+        }).session(session)
+
+        if (!toAccount) {
+            await session.abortTransaction()
+            return res.status(400).json({
+                message: "Accont not found"
+            })
+        }
+
+        await AccountModel.updateOne({ userId: (req as any).userId }, { $inc: { balance: -amount } }).session(session);
+        await AccountModel.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
+
+        await session.commitTransaction();
+
+        res.json({
+            message: "Transfer Successfully"
         })
+    } catch (error) {
+        await session.abortTransaction();
+        throw error;
+    } finally {
+        session.endSession();
     }
-
-    const toAccount = await AccountModel.findOne({
-        userId: to
-    }).session(session)
-
-    if (!toAccount) {
-        await session.abortTransaction()
-        res.status(400).json({
-            message: "Accont not found"
-        })
-    }
-
-    await AccountModel.updateOne({ userId: (req as any).userId }, { $inc: { balance: -amount } }).session(session);
-    await AccountModel.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
-
-    await session.commitTransaction();
-
-    res.json({
-        message: "Transfer Successfully"
-    })
 })
 
 export default accountRouter
